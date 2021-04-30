3b8a343daa405ee22be2723c9c0d9a602994d4e8
jr0dd <j_r0dd@icloud.com>
2021-04-29 19:45:47 -0700

    updated greeting

--- apps/discord-stock-ticker/shim/greeting.sh
+++ apps/discord-stock-ticker/shim/greeting.sh
@@ -4,7 +4,7 @@ echo "
 Welcome to the discord-stock-ticker container!
 
 To list current running bots run:
-curl localhost:8080/ticker
+curl localhost:8080/ticker | jq -r
 
 * Repository: https://github.com/jr0dd/container-images/
 * Repository: https://github.com/rssnyder/discord-stock-ticker
