195edf54427173e8482256db1c3ee007a3e6dfe1
jr0dd <285797+jr0dd@users.noreply.github.com>
2021-11-10 20:34:29 -0500

    add entrypoint

---
+++ apps/wyoa-bot/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cat << EOF > /app/config.json
+{
+    "token": "${TOKEN}"
+    "clientId": "904197696158572596",
+	"guildId": "779557077089124363" 
+}
+EOF
+
+exec node index.js
