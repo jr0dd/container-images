021b444da282da8c02e68dd2c4ae04ae3e301846
jr0dd <285797+jr0dd@users.noreply.github.com>
2021-11-21 23:52:56 -0500

    fix entrypoint

--- apps/wyoa-bot/entrypoint.sh
+++ apps/wyoa-bot/entrypoint.sh
@@ -8,7 +8,7 @@ cat << EOF > /app/config.json
   "clientId": "900047624781037598",
   "ownerID": "712785334903242834",
   "apiKeys": {
-	  "youtube": "${YOUTUBE_API}",
+    "youtube": "${YOUTUBE_API}",
     "finnhub": "${FINNHUB_API}",
     "iex": "${IEX_API}",
     "twitter": "${TWITTER_API}"
