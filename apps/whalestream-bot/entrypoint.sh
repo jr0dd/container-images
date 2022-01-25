827be8cd94f987ad59820e63f0f92f16a88c93ca
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-01-25 17:37:48 -0500

    test bot config.json as secret

--- apps/whalestream-bot/entrypoint.sh
+++ apps/whalestream-bot/entrypoint.sh
@@ -2,30 +2,4 @@
 
 set -eu
 
-cat << EOF > /app/config.json
-{
-  "token": "${DISCORD_TOKEN}",
-  "clientId": "927621519859929148",
-  "ownerID": "712785334903242834",
-  "apiKeys": {
-    "whalestream": {
-      "staging": "${WHALESTREAM_STAGING}",
-      "production": "${WHALESTREAM_PRODUCTION}"
-    },
-    "express": "${EXPRESS_JWT}",
-    "finnhub": "${FINNHUB_API}"
-  },
-  "webhooks": {
-    "premium": {
-      "highlyUnusual": "904074376796835881",
-      "unusual": "904080455157026917"
-    },
-    "free": {
-      "highlyUnusual": "904071329551089745",
-      "unusual": "904085056044273684"
-    }
-  }
-}
-EOF
-
-exec node index.js
+exec node /app/index.js
--- apps/wyoa-bot/entrypoint.sh
+++ apps/wyoa-bot/entrypoint.sh
@@ -2,26 +2,4 @@
 
 set -eu
 
-cat << EOF > /app/config.json
-{
-  "token": "${DISCORD_TOKEN}",
-  "clientId": "904197696158572596",
-  "ownerID": "712785334903242834",
-  "apiKeys": {
-    "whalestream": {
-      "staging": "${WHALESTREAM_STAGING}",
-      "production": "${WHALESTREAM_PRODUCTION}"
-    },
-    "express": "${EXPRESS_JWT}",
-    "youtube": "${YOUTUBE_API}",
-    "finnhub": "${FINNHUB_API}",
-    "iex": "${IEX_API}",
-    "twitter": "${TWITTER_API}"
-  },
-  "webhooks": {
-    "whalestream": "779557077235400711"
-  }
-}
-EOF
-
-exec node index.js
+exec node /app/index.js
