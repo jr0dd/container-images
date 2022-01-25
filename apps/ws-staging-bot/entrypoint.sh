1a51f23e2353021ea6efc59c4052cc4b4fc6a35f
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-01-25 17:02:26 -0500

    add config.json as secret

--- apps/ws-staging-bot/entrypoint.sh
+++ apps/ws-staging-bot/entrypoint.sh
@@ -2,30 +2,4 @@
 
 set -eu
 
-cat << EOF > /app/config.json
-{
-  "token": "${DISCORD_TOKEN}",
-  "clientId": "931366584394584164",
-  "ownerID": "712785334903242834",
-  "apiKeys": {
-    "whalestream": {
-      "staging": "${WHALESTREAM_STAGING}",
-      "production": "${WHALESTREAM_PRODUCTION}"
-    },
-    "express": "${EXPRESS_JWT}",
-    "finnhub": "${FINNHUB_API}"
-  },
-  "webhooks": {
-    "premium": {
-      "highlyUnusual": "931282876870307883",
-      "unusual": "931282876870307884"
-    },
-    "free": {
-      "highlyUnusual": "931307834535206983",
-      "unusual": "931307650795315291"
-    }
-  }
-}
-EOF
-
 exec node index.js
