35149fb7de2331cf381023f40661ae349e65b848
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-02-13 12:43:55 -0500

    ocd tweaks

--- apps/puppeteer/app.js
+++ apps/puppeteer/app.js
@@ -5,13 +5,13 @@ const puppeteer = require('puppeteer');
     headless: true,
     dumpio: true,
     args: [
-    //   '--font-render-hinting=medium',
       '--disable-gpu',
       '--disable-dev-shm-usage',
       '--disable-setuid-sandbox',
       '--no-sandbox',
       '--remote-debugging-port=4000',
 	    '--remote-debugging-address=0.0.0.0',
+      // '--font-render-hinting=medium',
       '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
     ]
   })
