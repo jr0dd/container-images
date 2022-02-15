4de72c2122ada0fce7abd9f215d10a8dc5a3c4f2
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-02-15 10:53:50 -0500

    add args under async function

--- apps/puppeteer/node/server.js
+++ apps/puppeteer/node/server.js
@@ -1,26 +1,25 @@
-const puppeteer = require('puppeteer')
-
-const args = process.argv.slice(2)
-
-const defaultArgs = [
-  '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS  X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/ 85.0.4183.121 Safari/537.36',
-  // required docker flags
-  '--disable-dev-shm-usage',
-  '--disable-setuid-sandbox',
-  '--no-sandbox',
-  // websocket config
-  '--remote-debugging-address=0.0.0.0',
-  '--remote-debugging-port=4000'
-]
-
-args.forEach(arg => {
-  defaultArgs.push(arg)
-});
+const puppeteer = require('puppeteer');
 
 (async () => {
+  const defaultArgs = [
+    '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS  X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/ 85.0.4183.121 Safari/537.36',
+    // required docker flags
+    '--disable-dev-shm-usage',
+    '--disable-setuid-sandbox',
+    '--no-sandbox',
+    // websocket config
+    '--remote-debugging-address=0.0.0.0',
+    '--remote-debugging-port=4000'
+  ]
+  const args = process.argv.slice(2)
+  args.forEach(arg => {
+    defaultArgs.push(arg)
+  })
+
   await puppeteer.launch({
     headless: true,
     dumpio: true,
+    ignoreHTTPSErrors: true,
     args: defaultArgs
   })
     .catch((err) => {
