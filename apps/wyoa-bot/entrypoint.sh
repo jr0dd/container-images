657d79edfd3a440bc5758ecd4bee2a0d24066fa7
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-04-05 00:50:39 -0400

    major refactor to a single codebase

--- apps/wyoa-bot/entrypoint.sh
+++ apps/wyoa-bot/entrypoint.sh
@@ -5,4 +5,14 @@ set -eu
 source "/shim/umask.sh"
 source "/shim/vpn.sh"
 
+SOPS_AGE_KEY=${SOPS_AGE_KEY}
+NODE_ENV=${NODE_ENV}
+
+cd /app/config
+rm *local*
+for i in *.yaml; do
+    sops -d -i $i
+    mv $i ${i/.sops.yaml/.yaml}
+done
+
 exec node /app/index.js
