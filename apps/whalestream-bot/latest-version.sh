01ec08f18693f467f2544a769970979d0b521c43
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-04-05 15:46:15 -0400

    remove whalestream & staging bots

--- apps/whalestream-bot/latest-version.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-version=$(curl -sX GET "https://api.github.com/repos/jr0dd/whalestream-bot/tags" -H "Authorization: token ${TOKEN}" | jq --raw-output '.[0].name')
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
--- apps/ws-staging-bot/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-source "/shim/umask.sh"
-source "/shim/vpn.sh"
-
-exec node /app/index.js
