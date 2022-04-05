d9f13280fbfcfdea88cb808e5e035fdae7d3198c
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-04-05 11:26:04 -0400

    fix entrypoint

--- apps/wyoa-bot/entrypoint.sh
+++ apps/wyoa-bot/entrypoint.sh
@@ -8,9 +8,8 @@ source "/shim/vpn.sh"
 SOPS_AGE_KEY=${SOPS_AGE_KEY}
 NODE_ENV=${NODE_ENV}
 
-cd /app/config
-rm *local*
-for i in *.yaml; do
+rm /app/config/*local*
+for i in /app/config/*.yaml; do
     sops -d -i $i
     mv $i ${i/.sops.yaml/.yaml}
 done
