eab5db93449d9179951a1dda19808041eac51be7
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-08-10 02:57:39 -0400

    update puppeteer

--- apps/puppeteer/node/server.js
+++ apps/puppeteer/node/server.js
@@ -3,15 +3,22 @@ import chalk from 'chalk'
 
 (async () => {
   const defaultArgs = [
-    '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS  X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/ 85.0.4183.121 Safari/537.36',
-    // required docker flags
     '--disable-dev-shm-usage',
     '--disable-setuid-sandbox',
     '--no-sandbox',
-    // websocket config
+    '--disable-audio-output',
+    '--disable-gpu',
+    '--no-zygote',
+    '--disable-software-rasterizer',
+    '--disable-features=IsolateOrigins,site-per-process',
+    '--disable-site-isolation-trials',
+    '--disable-web-security',
+    '--single-process',
+    '--use-gl=eg',
     '--remote-debugging-address=0.0.0.0',
     '--remote-debugging-port=4000'
   ]
+
   const args = process.argv.slice(2)
   args.forEach(arg => {
     defaultArgs.push(arg)
