c86b1eb1cd60f8fa6292cfd1003d1f32b85d0231
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-10-10 01:44:41 -0400

    fix release

--- base/ubuntu-focal/latest-version.sh
+++ base/ubuntu-focal/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl -s "https://registry.hub.docker.com/v1/repositories/library/ubuntu/tags" | jq --raw-output '.[] | select(.name | contains("focal")) | .name'  | tail -n1)
+version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/ubuntu/tags?ordering=name&name=focal" | jq --raw-output --arg s "focal" '.results[] | select(.name | contains($s)) | .name'  | head -n1)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
--- base/ubuntu-jammy/latest-version.sh
+++ base/ubuntu-jammy/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl -s "https://registry.hub.docker.com/v1/repositories/library/ubuntu/tags" | jq --raw-output '.[] | select(.name | contains("jammy")) | .name'  | tail -n1)
+version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/ubuntu/tags?ordering=name&name=jammy" | jq --raw-output --arg s "jammy" '.results[] | select(.name | contains($s)) | .name'  | head -n1)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
